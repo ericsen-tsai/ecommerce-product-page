1608db3047e1156c9ba4fb70c1d530e91577c655
Ericsen Tsai <bommer95175@gmail.com>
2022-07-03 21:36:11 +0800

    Add media query to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import { useMediaQuery } from "react-responsive"
 import Navbar from "./Navbar"
 
 import Hamburger from "../images/icon-menu.svg"
@@ -11,20 +11,23 @@ import "./Header.scss"
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const isTablet: boolean = useMediaQuery({ query: "(min-width: 37.5em)" })
   const [productQuantity, setProductQuantity] = useState<number>(1)
 
   return (
     <header className="header">
-      {isOpen && <Navbar toggleOpen={setIsOpen} />}
-      <div className="header__img-box">
-        <img
-          src={Hamburger}
-          alt="hamburger"
-          className="header__img header__img--hamburger"
-          onClick={() => setIsOpen(true)}
-        />
-      </div>
-      <div className="header__img-box header__img-box">
+      {(isOpen || isTablet) && <Navbar toggleOpen={setIsOpen} />}
+      {!isTablet && (
+        <div className="header__img-box">
+          <img
+            src={Hamburger}
+            alt="hamburger"
+            className="header__img header__img--hamburger"
+            onClick={() => setIsOpen(true)}
+          />
+        </div>
+      )}
+      <div className="header__img-box header__img-box--logo">
         <img src={Logo} alt="logo" className="header__img header__img--logo" />
       </div>
       <div className="header__img-box header__img-box--cart">
@@ -36,7 +39,11 @@ const Header = () => {
         )}
       </div>
       <div className="header__img-box">
-        <img src={Avatar} alt="avatar" className="header__img " />
+        <img
+          src={Avatar}
+          alt="avatar"
+          className="header__img header__img--avatar"
+        />
       </div>
     </header>
   )
